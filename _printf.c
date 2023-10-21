630c2acf752e61ca9b20950cc55f71a1ac19c4d4
Elethu <filtaneelethu@gmail.com>
2023-10-21 12:36:09 -0700

    update

--- _printf.c
+++ _printf.c
@@ -48,33 +48,3 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
-
-/**
- * _printf_string - print a string
- * @val: argument
- * Return: length of string
- */
-
-int _printf_string(va_list val)
-{
-	char *str;
-	int i;
-	int length;
-
-	str = va_arg(val, char *);
-	if (str == NULL)
-	{
-		str = "(NULL)";
-		length = _strlen(str);
-		for (i = 0; i < length; i++)
-			_putchar(str[i]);
-		return (length);
-	}
-	else
-	{
-		length = _strlen(str);
-		for (i = 0; i < length; i++)
-			_putchar(str[i]);
-		return (length);
-	}
-}
--- print_octal.c
+++ print_octal.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
  * print_octal - converts to octal
- * @val: variable parameter
- * @num: number 
+ * @val: variable parameter 
  * Return: counter
  */
 
@@ -10,8 +9,8 @@ int print_octal(va_list val)
 {
 	int i, counter = 0;
 	int *array;
-	unsigned int tem = num;
 	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
 
 	while (num / 8 != 0)
 	{
@@ -23,7 +22,7 @@ int print_octal(va_list val)
 
 	for ( i = 0; i < counter; i++)
 	{
-		array[i] = tem % 8;
+		array[i] = temp % 8;
 	}
 	for (i = counter - 1; i >= 0; i++)
 		_putchar(array[i] + '0');
